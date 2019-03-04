018db2596608c0f86adab3fa712075085270ab5b
Carol Gardiner <carolan.gardiner@gmail.com>
2019-03-04 12:08:19 +0000

    made constants for the magic numbers

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,27 +3,31 @@ class GildedRose
 
   def initialize(items)
     @items = items
-    @aged_brie = "Aged Brie"
-    @passes = "Backstage passes to a TAFKAL80ETC concert"
-    @sulfuras = "Sulfuras, Hand of Ragnaros"
+  
   end
+
+  AGED_BRIE = "Aged Brie"
+  PASSES = "Backstage passes to a TAFKAL80ETC concert"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
 
   def update_quality()
 
-
     @items.each do |item|
-      
-      if item.name != @aged_brie and item.name != @passes
+      #normal item 
+      if item.name != AGED_BRIE and item.name != PASSES
         if item.quality > 0
-          if item.name != @sulfuras
+          if item.name != SULFURAS
             item.quality = item.quality - 1
           end
         end
       
       else
+        #brie and backstage passes 
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == @passes
+
+          #backstage passes 
+          if item.name == PASSES
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -38,35 +42,37 @@ class GildedRose
         end
       end
       
-      if item.name != @sulfuras
+      #all other items 
+      if item.name != SULFURAS
         item.sell_in = item.sell_in - 1
       end
+
+   #how normal items have their quality decreased if expired 
       if item.sell_in < 0
-        if item.name != @aged_brie
-          if item.name != @passes
+        if item.name != AGED_BRIE
+          if item.name != PASSES
             if item.quality > 0
-              if item.name != @sulfuras
+              if item.name != SULFURAS
                 item.quality = item.quality - 1
               end
             end
+            #backstage pass goes to 0 when expired 
           else
             item.quality = item.quality - item.quality
           end
         else
+
           if item.quality < 50
             item.quality = item.quality + 1
           end
         end
       end
+
     end
   end
 
-  def normal_update_quality(name, quality, sell_in)
-    quality > 0 
-    quality = quality - 1
-
-  end
-
-
+def increase_item_quality(item)
+ 
+end
 
 end
